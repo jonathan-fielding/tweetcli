46694530de998f70dfaeb9ff561d4804f2b3fcf4
Jonathan Fielding <j.fielding@me.com>
2013-04-27 22:19:27 +0100

    trying to fix google analytics

--- scripts/social.js
+++ scripts/social.js
@@ -1,10 +1,6 @@
 /*global $, jQuery, alert, require, window, Backbone, define, Handlebars */
 /*jslint browser:true, devel:true, unused:false */
 
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-40491476-1']);
-_gaq.push(['_trackPageview']);
-
 /**
  * Load twitter widget javascript file.
  */
@@ -34,19 +30,6 @@ _gaq.push(['_trackPageview']);
 	js.src = '//connect.facebook.net/en_GB/all.js#xfbml=1&appId=136292643197914';
 	fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
-
-/**
- * Google analytics code
- */
-(function() {
-	'use strict';
-	var ga = document.createElement('script');
-	ga.type = 'text/javascript';
-	ga.async = true;
-	ga.src = ('https:' === document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-	var s = document.getElementsByTagName('script')[0];
-	s.parentNode.insertBefore(ga, s);
-})();
 
 /**
  * Google Plus Widget code
