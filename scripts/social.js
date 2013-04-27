4438e882cce0d2680455dec3b9f0be846da8a979
Jonathan Fielding <j.fielding@me.com>
2013-04-27 22:34:43 +0100

    try 3 at google analytics

--- scripts/social.js
+++ scripts/social.js
@@ -32,17 +32,6 @@
 }(document, 'script', 'facebook-jssdk'));
 
 /**
- * Google analytics code
- */
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-40491476-1', 'tweetcli.com');
-  ga('send', 'pageview');
-
-/**
  * Google Plus Widget code
  */
 
